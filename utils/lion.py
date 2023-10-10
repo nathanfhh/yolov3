51bf108493fa99b98b4d6c718f888bbb3fce4265
傅暄桓 <t13762@ms.sltung.com.tw>
2023-10-10 15:47:53 +0800

    reimplement lion

--- utils/lion.py
+++ utils/lion.py
@@ -1,89 +1,42 @@
-# Copyright 2023 Google Research. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""PyTorch implementation of the Lion optimizer."""
-import torch
-from torch.optim.optimizer import Optimizer
-
-
 class Lion(Optimizer):
-  r"""Implements Lion algorithm."""
+    # Based on - https://github.com/lucidrains/lion-pytorch/blob/main/lion_pytorch/lion_pytorch.py
+    def __init__(self, params, lr=1e-4, betas=(0.9, 0.99), weight_decay=0.0):
+        assert lr > 0.
+        assert all([0. <= beta <= 1. for beta in betas])
+        defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay)
 
-  def __init__(self, params, lr=1e-4, betas=(0.9, 0.99), weight_decay=0.0):
-    """Initialize the hyperparameters.
+        super().__init__(params, defaults)
 
-    Args:
-      params (iterable): iterable of parameters to optimize or dicts defining
-        parameter groups
-      lr (float, optional): learning rate (default: 1e-4)
-      betas (Tuple[float, float], optional): coefficients used for computing
-        running averages of gradient and its square (default: (0.9, 0.99))
-      weight_decay (float, optional): weight decay coefficient (default: 0)
-    """
+    @torch.no_grad()
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
 
-    if not 0.0 <= lr:
-      raise ValueError('Invalid learning rate: {}'.format(lr))
-    if not 0.0 <= betas[0] < 1.0:
-      raise ValueError('Invalid beta parameter at index 0: {}'.format(betas[0]))
-    if not 0.0 <= betas[1] < 1.0:
-      raise ValueError('Invalid beta parameter at index 1: {}'.format(betas[1]))
-    defaults = dict(lr=lr, betas=betas, weight_decay=weight_decay)
-    super().__init__(params, defaults)
+        for group in self.param_groups:
+            for p in group['params']:
 
-  @torch.no_grad()
-  def step(self, closure=None):
-    """Performs a single optimization step.
+                grad = p.grad
+                if grad is None:
+                    continue
 
-    Args:
-      closure (callable, optional): A closure that reevaluates the model
-        and returns the loss.
+                state = self.state[p]
+                lr, wd, beta1, beta2 = group['lr'], group['weight_decay'], *group['betas']
 
-    Returns:
-      the loss.
-    """
-    loss = None
-    if closure is not None:
-      with torch.enable_grad():
-        loss = closure()
+                # stepweight decay
+                p.data.mul_(1 - lr * wd)
 
-    for group in self.param_groups:
-      for p in group['params']:
-        if p.grad is None:
-          continue
+                # init state - exponential moving average of gradient values
+                if len(state) == 0:
+                    state['exp_avg'] = torch.zeros_like(p)
 
-        # Perform stepweight decay
-        p.data.mul_(1 - group['lr'] * group['weight_decay'])
+                exp_avg = state['exp_avg']
 
-        grad = p.grad
-        state = self.state[p]
-        # State initialization
-        if len(state) == 0:
-          # Exponential moving average of gradient values
-          state['exp_avg'] = torch.zeros_like(p)
+                # weight update
+                update = exp_avg.clone().lerp_(grad, 1 - beta1)
+                p.add_(torch.sign(update), alpha=-lr)
 
-        exp_avg = state['exp_avg']
-        beta1, beta2 = group['betas']
+                exp_avg.lerp_(grad, 1 - beta2)
 
-        # Weight update
-        update = exp_avg * beta1 + grad * (1 - beta1)
-
-        p.add_(torch.sign(update), alpha=-group['lr'], inplace=True)
-        #This has been made more efficient by using the torch.sign function's inplace mode. 
-        #This will prevent the need to create a new tensor for the updated parameter, 
-        #which can save a significant amount of time for large models. 
-
-        # Decay the momentum running average coefficient
-        exp_avg.mul_(beta2).add_(grad, alpha=1 - beta2)
-
-    return loss
+        return loss
